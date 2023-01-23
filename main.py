d60f1ea85656b0203850727b27c67a17bbec8298
fabianrusu <fabi.rusu@gmail.com>
2023-01-23 12:06:17 +0200

    Added minimum pasword length verification

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '!', '?']
-number_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-char_list = ['.', '!', '?']
+# number_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+# char_list = ['.', '!', '?']
 
 
 def pass_gen(number):
@@ -17,10 +17,13 @@ def pass_gen(number):
 
 
 def user_input():
-    print('>>>>>>>>>>Hello!<<<<<<<<<< \nWelcome to Password Generator v1.0')
-    number = input('Enter the length of your password: ')
-    return int(number)
+    print('>>>>>>>>>>Hello!<<<<<<<<<<\nWelcome to Password Generator v1.1')
+    length = input('Enter the length of your password: ')
+    while int(length) < 6:
+        print('Minimum password length is 6 characters.')
+        length = input('Enter the length of your password: ')
+    return int(length)
 
 
-user_num = user_input()
-print(pass_gen(user_num))
+user_pass = pass_gen(user_input())
+print(user_pass)
